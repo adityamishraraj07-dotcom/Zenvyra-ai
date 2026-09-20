d3d372a1ddb878ca8410644ae7dbacd66974f318
adityamishraraj07-dotcom <adityamishraraj07@gmail.com>
2026-09-20 18:58:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const navItems = document.querySelectorAll(".nav-item");
 
+navItems.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    navItems.forEach((nav) => {
+      nav.classList.remove("active");
+    });
+
+    item.classList.add("active");
+  });
+});
